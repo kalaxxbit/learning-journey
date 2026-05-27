54c5b4781010adb69515caa86b0a14f6906d9a86
kalaxxbit <kalaxxbit@gmail.com>
2026-05-27 14:53:17 +0300

    Course 05 : Problem 26 was solved

---
+++ abohadhoud/course-05/video-26/attempt.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include<cmath>
+#include <cstdlib>
+using namespace std;
+//FastLib Start
+int RandomNumbers(int From,int To){
+    int RandNum=rand()%(To-From+1)+From;
+    return RandNum;
+}
+// FastLib End
+int ReadPositiveNumber(string Text){
+    int Number=0;
+    do{
+    cout<<Text<<endl;
+    cin>>Number;
+    }while(Number<0);
+    return Number;
+
+}
+void PrintArray(int Arr[],int To){
+    
+    for(int i=0;i<To;i++){
+        cout<<Arr[i]<<" ";
+    }
+}
+
+void fillArrayWithRandomNumbers(int Array[100]){
+    for(int i=0;i<=99;i++){
+        Array[i]=RandomNumbers(1,100);
+    }
+}
+int GetSumOfArray(int Arr[100],int ArrLength){
+    int SumNumber=0;
+    for (int i=0;i<ArrLength;i++){
+     
+            SumNumber+=Arr[i];
+     
+    }
+    return SumNumber;
+}
+int main(int argc, char *argv[])
+{
+    srand((unsigned)time(NULL));
+    int Number=ReadPositiveNumber("Enter number of elements : ");
+    int Arr[100];
+    fillArrayWithRandomNumbers(Arr);
+    PrintArray(Arr,Number);
+    cout<<"\n Sum Number is "<<GetSumOfArray(Arr,Number)<<endl;
+
+}
---
+++ abohadhoud/course-05/video-26/problem.md
@@ -0,0 +1,22 @@
+#
+عبطا مث 100 ىلا 1 نم ةيئاوشع ماقرأب ، 100 ىصقأ مجحب ةفوفصم ءلمل اجمانرب بتكا
+ماقرلأا عومجم
+: تلاخدملا
+؟ لمعي اذام
+يئاوشع مقر
+ةيئاوشع ماقرلأ ةفوفصملا رصانع ةئبعت
+ةيئاوشعلا ماقرلأل ةفوفصملا رصانع ةعابط
+ةيئاوشعلا ماقرلأا عومجم ةعابط
+Enter number of elements : 5
+: تاجرخملا
+Array Elements : 12 34 67 97 56
+Sum Of all Number is : 266
+Function & Procedures …
+int RandomNumber
+void FillArrayWithRandomNumbers
+void PrintArray
+int SumArray
+م
+1
+2
+3
