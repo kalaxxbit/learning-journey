10cbec40243fd916e3011489c460262d120f9d45
kalaxxbit <kalaxxbit@gmail.com>
2026-06-19 19:20:56 +0300

    Course 08: Problem 03 was solved

---
+++ abohadhoud/review/course-08/video-03/problem.md
@@ -0,0 +1,9 @@
+Write a program to check if
+Year is a Leap Year or NOT
+Please enter a year to check ?
+1900
+No , Year [1900] is NOT a leap
+.year
+Please enter a year to check ?
+2000
+.Yes , Year [2000] is a leap year
---
+++ abohadhoud/review/course-08/video-03/solution.cpp
@@ -0,0 +1,25 @@
+#include "../../../lib/MySmallLibrary.h"
+#include <iostream>
+
+using namespace std;
+bool isLeapYear(short Year) {
+
+  return (Year % 4 == 0 && Year % 100 != 0) || (Year % 400 == 0);
+}
+int ReadYear() {
+  int Year = 0;
+  cout << "enter year: ";
+  cin >> Year;
+  return Year;
+}
+int main() {
+  int Year = ReadYear();
+  if (isLeapYear(Year)) {
+    cout << "Yes Year[" << Year << "] Is  a leap year" << endl;
+
+  } else {
+    cout << "No Year[" << Year << "] Is not a leap year" << endl;
+  }
+
+  return 0;
+}
