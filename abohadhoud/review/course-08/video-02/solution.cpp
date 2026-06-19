781303df25f57129176ad433f43370eb4afe1735
kalaxxbit <kalaxxbit@gmail.com>
2026-06-19 19:02:03 +0300

    Course 08: Problem 02 was solved

---
+++ abohadhoud/review/course-08/video-02/solution.cpp
@@ -0,0 +1,32 @@
+
+#include <iostream>
+
+using namespace std;
+bool isLeapYear(short Year) {
+  if (Year % 400 == 0) {
+    return true;
+  } else if (Year % 100 == 0) {
+    return false;
+  } else if (Year % 4 == 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+int ReadYear() {
+  int Year = 0;
+  cout << "enter year: ";
+  cin >> Year;
+  return Year;
+}
+int main() {
+  int Year = ReadYear();
+  if (isLeapYear(Year)) {
+    cout << "Yes Year[" << Year << "] Is  a leap year" << endl;
+
+  } else {
+    cout << "No Year[" << Year << "] Is not a leap year" << endl;
+  }
+  system("pause>0");
+  return 0;
+}
