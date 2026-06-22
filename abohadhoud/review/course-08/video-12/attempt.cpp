a6f7bf7b648870b9389ddd2812b739217a5a973f
kalaxxbit <kalaxxbit@gmail.com>
2026-06-22 19:50:45 +0300

    Course 08: Problem 12 was solved

---
+++ abohadhoud/review/course-08/video-12/attempt.cpp
@@ -0,0 +1,83 @@
+#include "../../../lib/MySmallLibrary.h"
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct sDate {
+  short Day = 1;
+  short Month = 1;
+  short Year = 1;
+};
+short ReadShortNumber(string msg) {
+  short Num;
+  cout << msg;
+  cin >> Num;
+  return Num;
+}
+short TotalDaysFromBeginningOfTheYear(sDate Date) {
+  short TotalDays = 0;
+  for (short i = 1; i < Date.Month; i++) {
+
+    TotalDays += NumberOfDaysInAMonth(i, Date.Year);
+  }
+  return TotalDays + Date.Day;
+}
+
+sDate ConvertYearDaysIntoDate(short Year, short Days) {
+  sDate Date;
+  short RemainingDays = Days;
+  short MonthDays = 0;
+  Date.Year = Year;
+  Date.Month = 1;
+  while (true) {
+    MonthDays = NumberOfDaysInAMonth(Date.Month, Year);
+    if (RemainingDays > MonthDays) {
+      RemainingDays -= MonthDays;
+      Date.Month++;
+    } else {
+      Date.Day = RemainingDays;
+      break;
+    }
+  }
+  return Date;
+}
+
+sDate AddingDaysToDate(sDate Date, short Days) {
+
+  short MonthDays = 0;
+  short RemainingDays = Days + TotalDaysFromBeginningOfTheYear(Date);
+  Date.Month = 1;
+  Date.Day = 1;
+  while (1) {
+    MonthDays = NumberOfDaysInAMonth(Date.Month, Date.Year);
+    if (RemainingDays > MonthDays) {
+      RemainingDays -= MonthDays;
+      Date.Month++;
+      if (Date.Month > 12) {
+        Date.Month = 1;
+        Date.Year++;
+      }
+    } else {
+      Date.Day = RemainingDays;
+      break;
+    }
+  }
+  return Date;
+}
+
+string ConvertDateToStringDate(sDate Date) {
+  return to_string(Date.Day) + "/" + to_string(Date.Month) + "/" +
+         to_string(Date.Year);
+}
+int main() {
+  sDate Date;
+
+  Date.Day = ReadShortNumber("Please enter a Day: ");
+  Date.Month = ReadShortNumber("Please enter a Month: ");
+  Date.Year = ReadShortNumber("Please enter a year: ");
+  short AddingDays = ReadShortNumber("how Much Days Want To Add: ");
+  cout << ConvertDateToStringDate(AddingDaysToDate(Date, AddingDays)) << endl;
+  return 0;
+}
---
+++ abohadhoud/review/course-08/video-12/problem.md
@@ -0,0 +1,12 @@
+Write a program to read
+how many days to add to it ,
+print the results on screen
+Please enter a Day to check ?
+14
+Please enter a Month to check
+? 8
+Please enter a year to check ?
+2023
+How many days to add? 2500
+Date after adding [2500] days
+is: 18/6/2030
