f833682bef954b30c633105f0da070e8015da88f
kalaxxbit <kalaxxbit@gmail.com>
2026-06-26 16:00:58 +0300

    Course 08: Problem 19 was solved

---
+++ abohadhoud/review/course-08/video-19/attempt.cpp
@@ -0,0 +1,163 @@
+#include "../../../lib/MySmallLibrary.h"
+
+#include <ctime>
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct sDate {
+  short Day = 1;
+  short Month = 1;
+  short Year = 1;
+};
+short ReadShortNumber(string msg) {
+  short Num;
+  cout << msg;
+  cin >> Num;
+  return Num;
+}
+short TotalDaysFromBeginningOfTheYear(sDate Date) {
+  short TotalDays = 0;
+  for (short i = 1; i < Date.Month; i++) {
+
+    TotalDays += NumberOfDaysInAMonth(i, Date.Year);
+  }
+  return TotalDays + Date.Day;
+}
+
+sDate ConvertYearDaysIntoDate(short Year, short Days) {
+  sDate Date;
+  short RemainingDays = Days;
+  short MonthDays = 0;
+  Date.Year = Year;
+  Date.Month = 1;
+  while (true) {
+    MonthDays = NumberOfDaysInAMonth(Date.Month, Year);
+    if (RemainingDays > MonthDays) {
+      RemainingDays -= MonthDays;
+      Date.Month++;
+    } else {
+      Date.Day = RemainingDays;
+      break;
+    }
+  }
+  return Date;
+}
+
+sDate AddingDaysToDate(sDate Date, short Days) {
+
+  short MonthDays = 0;
+  short RemainingDays = Days + TotalDaysFromBeginningOfTheYear(Date);
+  Date.Month = 1;
+  Date.Day = 1;
+  while (1) {
+    MonthDays = NumberOfDaysInAMonth(Date.Month, Date.Year);
+    if (RemainingDays > MonthDays) {
+      RemainingDays -= MonthDays;
+      Date.Month++;
+      if (Date.Month > 12) {
+        Date.Month = 1;
+        Date.Year++;
+      }
+    } else {
+      Date.Day = RemainingDays;
+      break;
+    }
+  }
+  return Date;
+}
+sDate ReadDate() {
+  sDate Date;
+
+  Date.Day = ReadShortNumber("Please enter a Day: ");
+  Date.Month = ReadShortNumber("Please enter a Month: ");
+  Date.Year = ReadShortNumber("Please enter a year: ");
+  return Date;
+}
+string ConvertDateToStringDate(sDate Date) {
+  return to_string(Date.Day) + "/" + to_string(Date.Month) + "/" +
+         to_string(Date.Year);
+}
+
+bool IsLastDayInonth(sDate Date1) {
+  return (Date1.Day == NumberOfDaysInAMonth(Date1.Month, Date1.Year));
+}
+
+bool IsDate1LessThanDate2(sDate Date1, sDate Date2) {
+
+  return (Date1.Year < Date2.Year)
+             ? true
+             : (Date1.Year == Date2.Year
+                    ? (Date1.Month < Date2.Month)
+                          ? true
+                          : (Date1.Month == Date2.Month
+                                 ? ((Date1.Day < Date2.Day))
+                                 : false)
+                    : false);
+}
+
+bool IsLastMonthInYear(sDate Date1) { return (Date1.Month == 12); }
+sDate IncreaseDateByOneDay(sDate Date) {
+  if (IsLastDayInonth(Date)) {
+    if (IsLastMonthInYear(Date)) {
+      Date.Year++;
+      Date.Month = 1;
+      Date.Day = 1;
+    } else {
+      Date.Month++;
+      Date.Day = 1;
+    }
+  } else {
+    Date.Day++;
+  }
+  return Date;
+}
+void SwapDates(sDate &Date1, sDate &Date2) {
+  sDate Temp = Date1;
+  Date1 = Date2;
+  Date2 = Temp;
+}
+short CalculateDifferencInDays(sDate Date1, sDate Date2,
+                               bool IncludingEndDay = 0) {
+  short Counter = 0;
+  bool Neg = false;
+  if (IsDate1LessThanDate2(Date2, Date1)) {
+    SwapDates(Date1, Date2);
+    Neg = true;
+  }
+
+  while (IsDate1LessThanDate2(Date1, Date2)) {
+    Date1 = IncreaseDateByOneDay(Date1);
+    Counter++;
+  }
+  Counter = (IncludingEndDay ? ++Counter : Counter);
+  return Neg ? Counter : Counter * -1;
+}
+
+sDate GetLocalDate() {
+  sDate LocalDate;
+  std::time_t currentTime = std::time(nullptr);
+  std::tm *localDate = std::localtime(&currentTime);
+  LocalDate.Year = localDate->tm_year + 1900;
+  LocalDate.Month = localDate->tm_mon + 1;
+  LocalDate.Day = localDate->tm_mday;
+  return LocalDate;
+}
+int main() {
+  sDate Date1 = ReadDate();
+  sDate Date2 = GetLocalDate();
+
+  cout << ".Difference is: " << CalculateDifferencInDays(Date1, Date2)
+       << " Day(s) " << endl;
+  cout << ".Difference  (Including End Day) is: "
+       << CalculateDifferencInDays(Date1, Date2, true) << " Day(s) " << endl;
+
+  cout << endl;
+
+  cout << ".Difference is: " << CalculateDifferencInDays(Date2, Date1)
+       << " Day(s) " << endl;
+  cout << ".Difference  (Including End Day) is: "
+       << CalculateDifferencInDays(Date2, Date1, true) << " Day(s) " << endl;
+
+  return 0;
+}
---
+++ abohadhoud/review/course-08/video-19/problem.md
@@ -0,0 +1,28 @@
+h,}
+bool IsLastMonthInYear(short Month)
+{
+return (Month == 12);
+}
+42
+Write a program to read
+Date1 , Date2 and make
+function to Calculate the
+Difference in Days
+NOTS : if Date2 is less than
+Date1 Print the Results in
+Minutes
+Please enter a Day to check ?
+16
+Please enter a Month to check
+? 8
+Please enter a year to check ?
+2023
+Please enter a Day to check ?
+13
+Please enter a Month to check
+? 12
+Please enter a year to check ?
+1999
+.Difference is: -8647 Day(s)
+Difference (Including End Day)
+.is: -8648 Day(s)
