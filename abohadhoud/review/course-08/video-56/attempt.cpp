f9584cbc1f7960262b64032f93425ddb01580c95
kalaxxbit <kalaxxbit@gmail.com>
2026-06-29 15:07:08 +0000

    Course 08: Problem 56 was solved

---
+++ abohadhoud/review/course-08/video-56/attempt.cpp
@@ -0,0 +1,196 @@
+#include "../../../lib/MySmallLibrary.h"
+
+#include <ctime>
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct sDate {
+  short Day = 1;
+  short Month = 1;
+  short Year = 1;
+};
+short ReadShortNumber(string msg) {
+  short Num;
+  cout << msg;
+  cin >> Num;
+  return Num;
+}
+short TotalDaysFromBeginningOfTheYear(sDate Date) {
+  short TotalDays = 0;
+  for (short i = 1; i < Date.Month; i++) {
+
+    TotalDays += NumberOfDaysInAMonth(i, Date.Year);
+  }
+  return TotalDays + Date.Day;
+}
+
+sDate ConvertYearDaysIntoDate(short Year, short Days) {
+  sDate Date;
+  short RemainingDays = Days;
+  short MonthDays = 0;
+  Date.Year = Year;
+  Date.Month = 1;
+  while (true) {
+    MonthDays = NumberOfDaysInAMonth(Date.Month, Year);
+    if (RemainingDays > MonthDays) {
+      RemainingDays -= MonthDays;
+      Date.Month++;
+    } else {
+      Date.Day = RemainingDays;
+      break;
+    }
+  }
+  return Date;
+}
+
+sDate ReadDate() {
+  sDate Date;
+
+  Date.Day = ReadShortNumber("Please enter a Day: ");
+  Date.Month = ReadShortNumber("Please enter a Month: ");
+  Date.Year = ReadShortNumber("Please enter a year: ");
+  return Date;
+}
+
+string ConvertDateToStringDate(sDate Date) {
+  return to_string(Date.Day) + "/" + to_string(Date.Month) + "/" +
+         to_string(Date.Year);
+}
+
+bool IsLastDayInonth(sDate Date1) {
+  return (Date1.Day == NumberOfDaysInAMonth(Date1.Month, Date1.Year));
+}
+
+bool IsDate1LessThanDate2(sDate Date1, sDate Date2) {
+
+  return (Date1.Year < Date2.Year)
+             ? true
+             : (Date1.Year == Date2.Year
+                    ? (Date1.Month < Date2.Month)
+                          ? true
+                          : (Date1.Month == Date2.Month
+                                 ? ((Date1.Day < Date2.Day))
+                                 : false)
+                    : false);
+}
+
+bool IsLastMonthInYear(sDate Date1) { return (Date1.Month == 12); }
+
+bool IsFristMonthInYear(sDate Date1) { return (Date1.Month == 1); }
+sDate IncreaseDateByOneDay(sDate Date) {
+  if (IsLastDayInonth(Date)) {
+    if (IsLastMonthInYear(Date)) {
+      Date.Year++;
+      Date.Month = 1;
+      Date.Day = 1;
+    } else {
+      Date.Month++;
+      Date.Day = 1;
+    }
+  } else {
+    Date.Day++;
+  }
+  return Date;
+}
+
+void SwapDates(sDate &Date1, sDate &Date2) {
+  sDate Temp = Date1;
+  Date1 = Date2;
+  Date2 = Temp;
+}
+
+short CalculateDifferencInDays(sDate Date1, sDate Date2,
+                               bool IncludingEndDay = 0) {
+  short Counter = 0;
+  bool Neg = false;
+  if (IsDate1LessThanDate2(Date2, Date1)) {
+    SwapDates(Date1, Date2);
+    Neg = true;
+  }
+
+  while (IsDate1LessThanDate2(Date1, Date2)) {
+    Date1 = IncreaseDateByOneDay(Date1);
+    Counter++;
+  }
+  Counter = (IncludingEndDay ? ++Counter : Counter);
+  return Neg ? Counter : Counter * -1;
+}
+
+sDate GetLocalDate() {
+  sDate LocalDate;
+  std::time_t currentTime = std::time(nullptr);
+  std::tm *localDate = std::localtime(&currentTime);
+  LocalDate.Year = localDate->tm_year + 1900;
+  LocalDate.Month = localDate->tm_mon + 1;
+  LocalDate.Day = localDate->tm_mday;
+  return LocalDate;
+}
+
+short DayOrderInDate(sDate Date) {
+  short a = (14 - Date.Month) / 12;
+
+  short y = Date.Year - a;
+
+  short m = Date.Month + (12 * a) - 2;
+
+  short d =
+      (Date.Day + y + (y / 4) - (y / 100) + (y / 400) + ((31 * m) / 12)) % 7;
+
+  return d;
+}
+
+string DayNameInOrder(short DayOrder) {
+  const string DaysNames[7] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+  return DaysNames[DayOrder];
+}
+
+string MonthNameInOrder(short MonthOrder) {
+  const string Months[] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun",
+                           "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+  return Months[MonthOrder - 1];
+}
+
+bool IsWeekEnd(sDate Date) {
+  // Weekends are Fri and Sat
+  short DayIndex = DayOrderInDate(Date);
+  return (DayIndex == 5 || DayIndex == 6);
+}
+sDate VacationReturnDate(sDate DateFrom,short VacationDays){
+	sDate VacationEndDate=DateFrom;
+	short Counter=VacationDays;
+	while(Counter>0){
+		if(!IsWeekEnd(VacationEndDate)){
+			Counter--;
+		}
+		
+		VacationEndDate=IncreaseDateByOneDay(VacationEndDate);
+	}
+	return VacationEndDate;
+	
+}
+
+
+
+
+
+bool IsDate1BigThanDate2(sDate Date1, sDate Date2) {
+
+  return !(IsDate1LessThanDate2(Date1,Date2));
+  
+}
+int main() {
+  
+  
+   
+  sDate Date1=ReadDate();
+  sDate Date2=ReadDate();
+  if(IsDate1BigThanDate2(Date1,Date2)){
+  	cout<<"Yes"<<endl;
+  }else{cout<<"No"<<endl;}
+
+
+
+
+  return 0;
+}
---
+++ abohadhoud/review/course-08/video-56/problem.md
@@ -0,0 +1,16 @@
+ Write a program to read  Date1 & Date2 and check if Date1 is after Date2 or Not
+ 
+   
+ : Enter Date1 Please enter a Day to check ?  1 
+ 
+ Please enter a Month to check ? 1
+ 
+ 
+ Please enter a year to check ? 2023  :
+  Enter Date2 Please enter a Day to check ? 1
+  
+   Please enter a Month to check ? 1 
+   
+   Please enter a year to check ? 2000 
+   
+     .Yes , Date1 is After Date2
