27e50e9dc1bc3062a28352d89d714dbcaa0afa07
kalaxxbit <kalaxxbit@gmail.com>
2026-08-19 16:07:08 +0300

    Adding: Implement clsAddress class with copy of address

---
+++ abohadhoud/progress/course-10/video-Copy_Constructors/attempt.cpp
@@ -0,0 +1,48 @@
+#include "../../../lib/MySmallLibrary.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+class clsAddress {
+private:
+  string _Address1;
+  string _Address2;
+  string _POBox;
+  string _ZipCode;
+
+public:
+  clsAddress(string Address1, string Address2, string POBox, string ZipCode) {
+    _Address1 = Address1;
+    _Address2 = Address2;
+    _POBox = POBox;
+    _ZipCode = ZipCode;
+  }
+  clsAddress(clsAddress &Obj) {
+    _Address1 = Obj.Address1();
+    _Address2 = Obj.Address2();
+    _POBox = Obj.POBox();
+    _ZipCode = Obj.ZipCode();
+  }
+  void PrintAddress() {
+    cout << "_Address1: " << _Address1 << endl;
+    cout << "_Address2: " << _Address2 << endl;
+    cout << "_POBox: " << _POBox << endl;
+    cout << "_ZipCode: " << _ZipCode << endl;
+  }
+  void SetAddress1(string Address1) { _Address1 = Address1; }
+  void SetAddress2(string Address2) { _Address2 = Address2; }
+  void SetPOBox(string POBox) { _POBox = POBox; }
+  void SetZipCode(string ZipCode) { _ZipCode = ZipCode; }
+
+  string Address1() { return _Address1; }
+  string Address2() { return _Address2; }
+  string POBox() { return _POBox; }
+  string ZipCode() { return _ZipCode; }
+};
+int main() {
+  clsAddress Address("Syria", "Raqqa", "1234", "1234");
+  clsAddress Address2(Address);
+  Address.PrintAddress();
+  Address2.PrintAddress();
+  return 0;
+}
