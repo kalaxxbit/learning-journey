6812bbb732e67c2858e12d2aa2b7d048a01b0348
kalaxxbit <kalaxxbit@gmail.com>
2026-08-27 22:03:18 +0300

    Adding: Implement initial version of attempt.cpp, notes.md, problem.md, and solution.cpp files for virtual functions demonstration

---
+++ abohadhoud/progress/course-10/video-Virtual_Functions/attempt.cpp
@@ -0,0 +1,30 @@
+#include "../../../lib/MySmallLibrary.h"
+#include <iostream>
+
+using namespace std;
+
+class clsPerson {
+
+public:
+  virtual void Print() { cout << "HI Im Person\n"; }
+};
+
+class clsEmployee : public clsPerson {
+public:
+  void Print() { cout << "HI Im  An Emp\n"; }
+};
+
+class clsStudent : public clsPerson {
+public:
+  void Print() { cout << "HI Im Student\n"; }
+};
+int main() {
+  clsEmployee Emp;
+  clsStudent Student;
+
+  clsPerson *Person1 = &Emp;
+  clsPerson *Person2 = &Student;
+  Person1->Print();
+  Person2->Print();
+  return 0;
+}
---
+++ abohadhoud/progress/course-10/video-Virtual_Functions/notes.md
@@ -0,0 +1,2 @@
+virtual Function :
+When You Make Virutal Func You TEll the complier to make virtual tbale in memory to saves addrs of that functon
